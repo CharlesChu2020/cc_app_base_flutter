4199e8cebc79fde449070eebff8f16451d0f2d01
Charles <zhucongbangonghao@outlook.com>
2021-04-09 11:52:40 +0800

    init project

---
+++ lib/cc_app_base.dart
@@ -0,0 +1,12 @@
+library cc_app_base;
+import 'dart:async';
+
+export 'network/http_util.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+
